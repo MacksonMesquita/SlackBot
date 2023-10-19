3ad928ffece687112b477f677ed1b0949681cd1a
Mackson Mesquita <129464904+MacksonMesquita@users.noreply.github.com>
2023-10-19 12:00:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,6 @@ import schedule
 import time
 from slack_sdk import WebClient
 
-#schedule.every(10).minutes.do(job)
-#schedule.every().hour.do(job)
-#schedule.every().day.at("10:30").do(job)
-#schedule.every().monday.do(job)
-#schedule.every().wednesday.at("13:15").do(job)
-#schedule.every().day.at("12:42", "Europe/Amsterdam").do(job)
-#schedule.every().minute.at(":17").do(job)
-# Schedule lib examples
-
 SLACK_TOKEN = "slack token" # Here, you will need the token generated through the slack API
 CHANNEL_ID = "id chanel"
 people_list = ["@personname", "@personname", "@personname", "@personname", "@personname"] 
